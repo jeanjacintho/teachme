553d77b42579d93d8d1a2bbd31fe2b116bf0ad13
jeanjacintho <jeanjacintho@yahoo.com.br>
2025-07-01 19:13:25 -0300

    feat: skeleton of main screen

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from "react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { GraduationCapIcon } from "lucide-react"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <GraduationCapIcon/>
+                <span className="text-base font-semibold">Teach Me.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        
+      </SidebarContent>
+      <SidebarFooter>
+       
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
